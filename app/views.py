5bc3a2069f599d43a339d91fa4163e50d0c4a8ae
israfil <israfil3019@gmail.com>
2021-07-09 17:17:35 +0300

    add template with context(variable)

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# def home_view(request):
+#     print(request.META)
+#     return HttpResponse("Hello World!")
 
 
 def home_view(request):
-
-    print(request.method)
-
-    return HttpResponse("Hello World!")
+    context = {
+        "first_name": "İsrafil",
+        "last_name": "T",
+    }
+    return render(request, "app/home.html", context)
